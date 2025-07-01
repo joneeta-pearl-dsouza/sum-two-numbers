d8d8bb29ca796ae05f00358dd16f59264b50fcd4
joneeta-pearl-dsouza <joneetapearldsouza@gmail.com>
2025-07-01 21:11:53 +0530

    Split login and home into components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [name, setName] = useState("");
+
+  const handleLoginSuccess = (userName) => {
+    setName(userName);
+    setIsLoggedIn(true);
+  };
+
+  return isLoggedIn ? (
+    <Home name={name} />
+  ) : (
+    <Login onLoginSuccess={handleLoginSuccess} />
   );
-}
+};
 
 export default App;
